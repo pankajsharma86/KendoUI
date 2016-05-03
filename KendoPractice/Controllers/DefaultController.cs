dad334ad4e8deeab50b4e97549862b69cc2549f9
Pankaj Sharma <p.sharma511@yahoo.co.nz>
2016-05-03 15:11:55 +1200

    Read operation from table named "customers"

---
+++ KendoPractice/Controllers/DefaultController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KendoPractice.Models;
+
+namespace KendoPractice.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public JsonResult GetUserList()
+        {
+            using (var db = new StoreEntities1())
+            {
+                var users = db.Customers.Select(c => new
+                {
+                    
+                    Name = c.Name,
+                    City = c.City,
+                    Address = c.Address,
+                    CustomerId = c.CustomerId,
+                    CityId = c.CityId,
+                    Country = c.Country,
+                    Created = c.Created,
+                 }).ToList();
+
+                return Json(users, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+    }
+}
